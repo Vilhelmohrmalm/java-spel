335faf94767b3bb18c37b909504829f4a7dd51ba
vilhelmohrmalm <vilhelm.ohrmalm@skoal.taby.se>
2023-05-08 15:27:51 +0200

    hej

--- canvas.js
+++ canvas.js
@@ -1,3 +1,130 @@
+function statgen(a, b){
+    return(Math.round(Math.random()*b+a))
+    }
+
+
+
+function vapengenerator(spelar_stats){
+    if( spelar_stats.p_lvl < 10){
+        let Svärd = new Vapen(statgen(0, 1), statgen(1, 2), "ett Svärd")
+        let Sköld = new Vapen(statgen(1, 2), statgen(0, 0), "en Sköld")
+        let Yxa = new Vapen(statgen(0, 0), statgen(1, 4), "en Yxa")
+        let Pilbåge = new Vapen(statgen(0, 0), statgen(1, 3), "en Pilbåge")
+        let Spjut = new Vapen(statgen(0, 0), statgen(2, 3), "ett Spjut")
+        let Vlista = [Svärd, Sköld, Yxa, Pilbåge, Spjut]
+        let index = Math.floor(Math.random() * Vlista.length);
+        let vapen = Vlista[index]
+        return(vapen)
+    
+    }
+    else if (spelar_stats.p_lvl < 20){
+        let Svärd = new Vapen(statgen(0, 1), statgen(2, 4), "ett Svärd")
+        let Sköld = new Vapen(statgen(2, 3), statgen(0, 0), "en Sköld")
+        let Yxa = new Vapen(statgen(0, 0), statgen(1, 6), "en Yxa")
+        let Pilbåge = new Vapen(statgen(0, 0), statgen(2, 5), "en Pilbåge")
+        let Spjut = new Vapen(statgen(0, 0), statgen(3, 4), "ett Spjut")
+        let Vlista = [Svärd, Sköld, Yxa, Pilbåge, Spjut]
+        let index = Math.floor(Math.random() * Vlista.length);
+        let vapen = Vlista[index]
+        return(vapen)
+    }
+    else if(spelar_stats.p_lvl < 30){
+        let Svärd = new Vapen(statgen(1, 1), statgen(4, 5), "ett Svärd")
+        let Sköld = new Vapen(statgen(3, 4), statgen(0, 0), "en Sköld")
+        let Yxa = new Vapen(statgen(0, 0), statgen(3, 8), "en Yxa")
+        let Pilbåge = new Vapen(statgen(0, 0), statgen(4, 7), "en Pilbåge")
+        let Spjut = new Vapen(statgen(0, 0), statgen(5, 6), "ett Spjut")
+        let Vlista = [Svärd, Sköld, Yxa, Pilbåge, Spjut]
+        let index = Math.floor(Math.random() * Vlista.length);
+        let vapen = Vlista[index]
+        return(vapen)
+    }
+}
+    
+
+
+
+
+class Vapen {
+        constructor(v_namn,v_hp,v_str){
+        this.v_namn = v_namn;
+        this.v_hp = v_hp;
+        this.v_str = v_str;}
+    
+}
+
+
+class Karaktärer{
+    constructor( p_hp, p_str, p_lvl, p_lvlpoäng, p_namn, vapen){
+    this.p_hp = p_hp;
+    this.p_str = p_str;
+    this.p_lvl = p_lvl;
+    this.p_lvlpoäng = p_lvlpoäng;
+    this.p_namn = p_namn;
+    this.vapen = vapen; }
+}
+
+class Monster{
+    constructor( m_hp, m_str, m_namn){
+        this.m_namn = m_namn;
+        this.m_hp = m_hp;
+        this.m_str = m_str;}
+    //def __str__(self):
+      //  return f"Du stöter på en {self.m_namn}, den har {self.m_hp} hp och {self.m_str} styrka"
+}
+
+function monstrgenerator(){
+    monster = Math.round(Math.random())
+    if (Karaktärer.p_lvl < 10){
+        let Slime = new Monster(statgen(4, 11), statgen(2, 6), "Slime")
+        let Goblin = new Monster(statgen(3, 7), statgen(3, 8), "Goblin")
+        if (monster == 1){
+        monster_stats = (Slime);
+        return (monster_stats);
+        }
+        else if (monster == 2){
+        monster_stats = (Goblin)
+        return (monster_stats)
+        }
+    }
+
+    else if (Karaktärer.p_lvl < 20){
+        let Lycan = new Monster(statgen(6, 12), statgen(4, 9), "Lycan")
+        let Golem = new Monster(statgen(12, 17), statgen(3,3), "Golem")  
+        if (monster == 1){
+        monster_stats = (Lycan);
+        return (monster_stats);
+        }
+        else if (monster == 2){
+        monster_stats = (Golem)
+        return (monster_stats)
+        }
+    }
+    else (Karaktärer.p_lvl < 30);{
+        let Undead = new Monster(health(15, 22), damage(4, 5), "Undead")
+        let Orc = new Monster(health(9, 15), damage(6, 10), "Orc") 
+        if (monster == 1){
+        monster_stats = (Undead);
+        return (monster_stats);
+        }
+        else if (monster == 2){
+        monster_stats = (Orc)
+        return (monster_stats)
+        }
+    }
+}
+
+
+let Start = new Vapen(0, 0, "en pinne")
+
+let Assasin = new Karaktärer(5, 10, 0, 0, "assasin", Start)
+let Barb = new Karaktärer(8, 7, 0, 0, "Barb", Start)
+let Knight = new Karaktärer(10, 5, 0, 0, "Knight", Start)
+
+
+
+
+
 let spelar_stats;
 
 function start(){
@@ -11,8 +138,18 @@ function assa(){
  var val = document.getElementById("karaktärsval")
  val.style.display = "none"
  spelar_stats = Asssin;
+}
 
+function barb(){
+    var val = document.getElementById("karaktärsval")
+    val.style.display = "none"
+    spelar_stats = Barb;
+}
 
+function knig(){
+    var val = document.getElementById("karaktärsval")
+    val.style.display = "none"
+    spelar_stats = knig;
 }
 
 console.log(spelar_stats)
